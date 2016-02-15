05c9397bf45267a992ee7703986880ac091acc15
AleksandrFrolov <alfrobox@gmail.com>
2016-02-15 17:35:36 +0300

    two or more rings fix

--- dirac/smiles_tools.py
+++ dirac/smiles_tools.py
@@ -3,23 +3,35 @@ from enum import Enum
 import re
 from rdkit.Chem import MolFromSmiles, MolFromSmarts, MolToSmiles
 from rdkit.Chem import FragmentOnBonds
+from rdkit.Chem import BRICS
+
+def is_cyclic(smiles):
+    return len([a for a in MolFromSmiles(smiles).GetAtoms() if a.IsInRing()]) > 0
 
 def molecules_fragmentation(smiles, template):
     """
     Separate molecule on fragments.
     :param smiles: smiles string
-    :param template: smart template [!R][R]
-    :return: list of ChemFragment's
+    :param template: smart template, for example [!R][R]
+    :return: ChemFragment list
     """
     smiles = MolFromSmiles(smiles)
     bis = smiles.GetSubstructMatches(MolFromSmarts(template))
     bs = [smiles.GetBondBetweenAtoms(x, y).GetIdx() for x, y in bis]
     nm = FragmentOnBonds(smiles, bs)
-    frags =MolToSmiles(nm, True).split('.')
-    frag_name = lambda s: re.sub('\(*\[+\d*\*+\]+\)*', '', s)
+    frags = []
+    for f in MolToSmiles(nm, True).split('.'):
+        if not is_cyclic(f):
+            frags.append(f)
+        else:
+            if MolFromSmiles(f).GetRingInfo().NumRings() == 1:
+                frags.append(f)
+            else:
+                for i in BRICS.BRICSDecompose(MolFromSmiles(f)):
+                    frags.append(i)
+    frag_name = lambda s: MolToSmiles(MolFromSmiles(re.sub('\(*\[+\d*\*+\]+\)*', '', s)))
     frag_smiles = lambda s: re.sub('\[+\d*\*+\]+', '[*]', s)
-    frag_type = lambda s: FragmentType.heterocycle \
-        if len([a for a in MolFromSmiles(s).GetAtoms() if a.IsInRing()]) > 0 \
+    frag_type = lambda s: FragmentType.heterocycle if is_cyclic(s) \
         else FragmentType.joiner if s.count('*') == 2 else FragmentType.attacher
     return [ChemFragment(frag_name(s), frag_smiles(s), frag_type(s)) for s in frags]
 
@@ -30,3 +42,5 @@ class FragmentType(Enum):
     attacher = 'ATTACHER'
 
 ChemFragment = namedtuple('ChemFragment', 'name, smiles, type')
+
+print '.'.join(map(lambda s: s.smiles, molecules_fragmentation('C1CCN[C@@H](C1)C2(CN(C2)C(=O)C3=C(C(=C(C=C3)F)F)NC4=C(C=C(C=C4)I)F)O', '[!R][R]')))
